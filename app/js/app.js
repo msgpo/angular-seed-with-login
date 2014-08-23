f31dee80cf86439d64d5ab33cc508be6653d2228
Yongzhi Huang <yhuang@fastcompany.com>
2014-08-23 16:38:59 -0400

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,22 @@
 'use strict';
-
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var app= angular.module('myApp', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 }]);
+
+
+app.run(function($rootScope, $location, loginService){
+	var routespermission=['/home'];  //route that require login
+	$rootScope.$on('$routeChangeStart', function(){
+		if( routespermission.indexOf($location.path()) !=-1)
+		{
+			var connected=loginService.islogged();
+			connected.then(function(msg){
+				if(!msg.data) $location.path('/login');
+			});
+		}
+	});
+});
